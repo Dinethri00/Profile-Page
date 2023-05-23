3fc79f22809f705c3f931abd05ab0186f47f0580
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-05-23 19:23:48 +0530

    Create Profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,397 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_profile_picture/flutter_profile_picture.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FLUTTER PROFILE PICTURE',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Example FPP'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Container(
+            width: 500,
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Table(
+              border: TableBorder.all(),
+              columnWidths: const <int, TableColumnWidth>{
+                0: FlexColumnWidth(),
+                1: FlexColumnWidth(),
+                2: FlexColumnWidth(),
+              },
+              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+              children: <TableRow>[
+                _exampleDefault(),
+                _exampleRandomColor(),
+                _exampleMaxLetter(),
+                _exampleEmptyName(),
+                _exampleImages(),
+                _exampleTooltipsWithoutRole(),
+                _exampleTooltipsWithRole(),
+                _exampleTooltipsWithRoleAndImages(),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  TableRow _exampleTooltipsWithRoleAndImages() {
+    return TableRow(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Text(
+            '8. Tooltips with role and images',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          // enable tooltip feature with role and image
+          // same as example 7
+          // but you can add parameter img = 'your_img'
+          // background color and initial name will be replaced with the image
+          child: ProfilePicture(
+            name: 'Aditya Dharmawan Saputra',
+            role: 'ADMINISTRATOR',
+            radius: 31,
+            fontsize: 21,
+            tooltip: true,
+            img: 'https://avatars.githubusercontent.com/u/37553901?v=4',
+          ),
+        ),
+        Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Text(
+              'Replace name with image, you can click the images if you want to show tooltips with full name and role',
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  TableRow _exampleTooltipsWithRole() {
+    return TableRow(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Text(
+            '7. Tooltips with role',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          // enable tooltip feature with role
+          // same as example 6
+          // but you can add parameter role = 'your_role'
+          // role will be appear under name
+          child: ProfilePicture(
+            name: 'Aditya Dharmawan Saputra',
+            role: 'ADMINISTRATOR',
+            radius: 31,
+            fontsize: 21,
+            tooltip: true,
+          ),
+        ),
+        Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Text(
+              'Click to show tooltips with full name and role',
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  TableRow _exampleTooltipsWithoutRole() {
+    return TableRow(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Text(
+            '6. Tooltips without role',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          // enable tooltip feature
+          // set tooltip = true
+          // default is false
+          // when you click the picture
+          // name will be appear
+          child: ProfilePicture(
+            name: 'Aditya Dharmawan Saputra',
+            role: '',
+            radius: 31,
+            fontsize: 21,
+            tooltip: true,
+          ),
+        ),
+        Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Text(
+              'Click to show tooltips with full name',
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  TableRow _exampleImages() {
+    return TableRow(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Text(
+            '5. Images',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          // change background color to image
+          // set img = 'your_url_img'
+          child: ProfilePicture(
+            name: 'Aditya Dharmawan Saputra',
+            radius: 31,
+            fontsize: 21,
+            img: 'https://avatars.githubusercontent.com/u/37553901?v=4',
+          ),
+        ),
+        Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Text(
+              'add "img:your_url" for replace the name with images',
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  TableRow _exampleEmptyName() {
+    return TableRow(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Text(
+            '4. Empty Name',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          // for handling user without name
+          // set name = '' (empty string)
+          // will set default background color without name
+          child: ProfilePicture(
+            name: '',
+            radius: 31,
+            fontsize: 21,
+          ),
+        ),
+        Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Text(
+              'You can make a perfect circle without filling the name',
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  TableRow _exampleMaxLetter() {
+    return TableRow(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Text(
+            '3. Max Letter',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          // set limit letter
+          // use count = 'your_limit'
+          // default is 2 if more than one words
+          // default is 1 if less than two words
+          child: ProfilePicture(
+            name: 'Aditya Dharmawan Saputra',
+            radius: 31,
+            fontsize: 21,
+            count: 3,
+          ),
+        ),
+        Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Text(
+              'add "count:your_length" for set how much the letters shown on circle',
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  TableRow _exampleRandomColor() {
+    return TableRow(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Text(
+            '2. Random Color',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          // set random = true
+          // default is false
+          child: ProfilePicture(
+            name: 'Aditya Dharmawan Saputra',
+            radius: 31,
+            fontsize: 21,
+            random: true,
+          ),
+        ),
+        Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Text(
+              'add random:true if you want to give random color',
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  TableRow _exampleDefault() {
+    return TableRow(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Text(
+            '1. Default',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Center(
+          child: Container(
+            child: Row(
+              children: [
+                // if there is only one word.
+                ProfilePicture(
+                  name: 'Dees',
+                  radius: 31,
+                  fontsize: 21,
+                ),
+                SizedBox(width: 5),
+                // if more than one word.
+                ProfilePicture(
+                  name: 'Aditya Dharmawan Saputra',
+                  radius: 31,
+                  fontsize: 21,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Text(
+            'This is default style. Default color based on First Initial Name',
+            style: TextStyle(
+              fontSize: 12,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
